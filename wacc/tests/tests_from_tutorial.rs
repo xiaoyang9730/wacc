5be66535d91f69760892c389111576ac8645c4eb
cheng <xiaoyang9730@outlook.com>
2024-10-01 22:32:45 +0800

    Rework on test outputs, print test script outputs when failed

--- wacc/tests/tests_from_tutorial.rs
+++ wacc/tests/tests_from_tutorial.rs
@@ -9,7 +9,7 @@ enum OutputType {
 const TEST_COMPILER: &str = "../wacct/test_compiler";
 const WACC: &str = "target/debug/wacc";
 
-fn test_compiler(args: &[&str], get_output: OutputType) -> String {
+fn test_compiler(args: &[&str], get_output: OutputType) -> (bool, String) {
     print!("{TEST_COMPILER}");
     for arg in args {
         print!(" {arg}");
@@ -17,49 +17,57 @@ fn test_compiler(args: &[&str], get_output: OutputType) -> String {
     println!("");
 
     let output = Command::new(TEST_COMPILER).args(args).output().expect("That `test_compiler` command should be executed");
-    assert!(output.status.success());
-    match get_output {
-        Stdout => String::from_utf8(output.stdout).expect("That `test_compiler` should output UTF-8"),
-        Stderr => String::from_utf8(output.stderr).expect("That `test_compiler` should output UTF-8"),
-    }
+    (
+        output.status.success(),
+        match get_output {
+            Stdout => String::from_utf8(output.stdout).expect("That `test_compiler` should output UTF-8"),
+            Stderr => String::from_utf8(output.stderr).expect("That `test_compiler` should output UTF-8"),
+        },
+    )
 }
 
 fn check_setup() {
     println!("--- CH 0: CHECK SETUP ---");
-    let output = test_compiler(&["--check-setup"], Stdout);
+    let (success, output) = test_compiler(&["--check-setup"], Stdout);
     println!("{output}");
     assert_eq!(output, "All system requirements met!\n");
+    assert!(success);
 }
 
 fn chapter_1() {
     println!("--- CH 1: A MINIMAL COMPILER ---");
     println!("[LEX]");
-    let output = test_compiler(&[WACC, "--chapter", "1", "--stage", "lex"], Stderr);
+    let (success, output) = test_compiler(&[WACC, "--chapter", "1", "--stage", "lex"], Stderr);
     println!("{output}");
     assert!(output.ends_with("OK\n"));
+    assert!(success);
 
     println!("[PARSE]");
-    let output = test_compiler(&[WACC, "--chapter", "1", "--stage", "parse"], Stderr);
+    let (success, output) = test_compiler(&[WACC, "--chapter", "1", "--stage", "parse"], Stderr);
     println!("{output}");
     assert!(output.ends_with("OK\n"));
+    assert!(success);
 
     println!("[CODEGEN]");
-    let output = test_compiler(&[WACC, "--chapter", "1", "--stage", "codegen"], Stderr);
+    let (success, output) = test_compiler(&[WACC, "--chapter", "1", "--stage", "codegen"], Stderr);
     println!("{output}");
     assert!(output.ends_with("OK\n"));
+    assert!(success);
 
     println!("[WHOLE COMPILER]");
-    let output = test_compiler(&[WACC, "--chapter", "1"], Stderr);
+    let (success, output) = test_compiler(&[WACC, "--chapter", "1"], Stderr);
     println!("{output}");
     assert!(output.ends_with("OK\n"));
+    assert!(success);
 }
 
 fn chapter_2() {
     println!("--- CH 2: Unary Operators ---");
     println!("[LEX]");
-    let output = test_compiler(&[WACC, "--chapter", "2", "--stage", "lex"], Stderr);
+    let (success, output) = test_compiler(&[WACC, "--chapter", "2", "--stage", "lex"], Stderr);
     println!("{output}");
     assert!(output.ends_with("OK\n"));
+    assert!(success);
 }
 
 #[test]
